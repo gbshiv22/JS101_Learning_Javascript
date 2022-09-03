0e87687151cf8a9b8e4f0005cf9653964303cfc1
gbshiv22 <shivdegb1999@gmail.com>
2022-09-04 00:48:49 +0530

    Added Mathematical Operator file to jscript folder

---
+++ MathematicalOperators/problem3.js
@@ -0,0 +1,17 @@
+// The value stored in the variable`two, five, six`has been doubled, such that if initially the value was 3, it has become 6
+
+//The value stored in the variable`three, four`has been increased three times, such that if initially it was 3, it has become 9
+
+// The value stored in the variable `one` has not been changed.
+
+// Find the sum of the updated values stored in`one, two, three, four, five, six`
+
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+let product = ((2*(two+five+six))+(3*(three+four))+one);
+
+console.log(product);
